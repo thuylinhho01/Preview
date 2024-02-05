9854c9ade603d08de663f8bf5dba19f7bf596a34
thuylinhho01 <hothithuylinh17092001@gmail.com>
2024-02-06 02:50:44 +0700

    update

---
+++ assets/script/script-highlight.js
@@ -0,0 +1,13 @@
+
+document.getElementById('next').onclick = function(){
+    const widthItem = document.querySelector('.card').offsetWidth;
+    document.getElementById('formList').scrollLeft += widthItem;
+}
+
+document.getElementById('prev').onclick = function(){
+    const widthItem = document.querySelector('.card').offsetWidth;
+    document.getElementById('formList').scrollLeft -= widthItem;
+}
+
+
+
